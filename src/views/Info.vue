c9909b771ca50f87aa3f50f8d03f5bbbbe1ba8fe
Sergio Fores <sergiofores@gmail.com>
2020-04-19 01:14:30 +0200

    fix

---
+++ src/views/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="info">
+    <Brand />
+    <Footer msg="Sergio Fores" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Brand from "@/components/Brand.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Info",
+  components: {
+    Brand,
+    Footer
+  },
+};
+</script>
