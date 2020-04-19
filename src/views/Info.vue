24c3a76f04e834a8be124f3f9e61a31d2305cf87
Sergio Fores <sergiofores@gmail.com>
2020-04-19 04:29:47 +0200

    deploy

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="info">
+    <Cover msg="Todo es relación" />
+
     <Brand />
+
     <Footer msg="Sergio Fores" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Cover from "@/components/Cover.vue";
 import Brand from "@/components/Brand.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Info",
   components: {
+    Cover,
     Brand,
-    Footer
+    Footer,
   },
 };
 </script>
